5e81f04ce9a62d37e21eee3eacd0322fcb654e29
Ouchekkir Abdelmouaine <abdelmouaineouchekkir@gmail.com>
2020-04-10 14:14:07 +0100

    Create namecounter.py

---
+++ extplugins/namecounter.py
@@ -0,0 +1,32 @@
+__version__ = '1.0'
+__author__ = 'Zwambro'
+
+import b3
+import b3.events
+import b3.plugin
+from b3 import functions
+import re
+class NamecounterPlugin(b3.plugin.Plugin):
+    _adminPlugin = False
+    requiresConfigFile = False
+
+    def onStartup(self):
+
+        self._adminPlugin = self.console.getPlugin('admin')
+        if not self._adminPlugin:
+            self.debug('Could not find admin plugin')
+            return False
+        else:
+            self.debug('Plugin loaded normal')
+        self.registerEvent(b3.events.EVT_CLIENT_AUTH)
+        self.debug('Started')
+
+    def stripColors(self, s):
+        return re.sub('\^[0-9]{1}', '', s)
+
+    def onEvent(self, event):
+        if event.type == b3.events.EVT_CLIENT_AUTH:
+            name = self.stripColors(event.client.name)
+            if len(name) < 3:
+                self.debug("(%s) have less than 2 characters in his name" %(name))
+                event.client.kick("Less player name characters")
